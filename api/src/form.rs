8e3d2aee2ed065ec99eb3e799f814b9c3cd91e36
Sanpi <sanpi@homecomputing.fr>
2020-12-03 12:25:39 +0100

    [api] Improves source update error handling

--- api/src/form.rs
+++ api/src/form.rs
@@ -13,23 +13,50 @@ impl std::convert::TryInto<crate::model::source::Entity> for Source {
     type Error = crate::Error;
 
     fn try_into(self) -> crate::Result<crate::model::source::Entity> {
-        let contents = attohttpc::get(&self.url).send()?.text()?;
-        let feed = feed_rs::parser::parse(contents.as_bytes())?;
-
         let user_id = match self.user_id {
             Some(user_id) => user_id,
             None => return Err(crate::Error::Auth),
         };
 
-        let mut title = self.title;
+        let title = match self.title {
+            Some(title) => title,
+            None => self.title().unwrap_or_else(|| "<no title>".to_string()),
+        };
 
-        if title.is_none() {
-            title = feed.title.map(|x| x.content);
-        }
+        let entity = crate::model::source::Entity {
+            last_error: None,
+            source_id: self.source_id,
+            tags: self.tags,
+            title,
+            url: self.url.clone(),
+            user_id,
+        };
+
+        Ok(entity)
+    }
+}
+
+impl Source {
+    fn title(&self) -> Option<String> {
+        let contents = match Self::fetch(&self.url) {
+            Ok(contents) => contents,
+            Err(_) => return None,
+        };
+
+        let feed = match feed_rs::parser::parse(contents.as_bytes()) {
+            Ok(feed) => feed,
+            Err(_) => return None,
+        };
+
+        let mut title = feed.title.map(|x| x.content);
 
         if title.is_none() {
             for link in feed.links {
-                let contents = attohttpc::get(&link.href).send()?.text()?;
+                let contents = match Self::fetch(&link.href) {
+                    Ok(contents) => contents,
+                    Err(_) => continue,
+                };
+
                 let html = scraper::Html::parse_document(&contents);
 
                 let selector = scraper::Selector::parse("head title").unwrap();
@@ -41,15 +68,12 @@ impl std::convert::TryInto<crate::model::source::Entity> for Source {
             }
         }
 
-        let entity = crate::model::source::Entity {
-            last_error: None,
-            source_id: self.source_id,
-            tags: self.tags,
-            title: title.unwrap_or_else(|| "<no title>".to_string()),
-            url: self.url.clone(),
-            user_id,
-        };
+        title
+    }
 
-        Ok(entity)
+    fn fetch(url: &str) -> Result<String, attohttpc::Error> {
+        attohttpc::RequestBuilder::try_new(attohttpc::Method::GET, url)?
+            .send()?
+            .text()
     }
 }
