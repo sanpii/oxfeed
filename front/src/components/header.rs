30a092531c18f0ae67ebacc12c0779f948b966ff
Sanpi <sanpi@homecomputing.fr>
2022-03-22 09:54:25 +0100

    Fixes burger menu

--- front/src/components/header.rs
+++ front/src/components/header.rs
@@ -50,7 +50,7 @@ impl yew::Component for Component {
         yew::html! {
             <>
                 <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">{{ "Oxfeed" }}</a>
-                <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <super::search::Bar current_route={ self.current_route.clone() } filter={ filter } />
