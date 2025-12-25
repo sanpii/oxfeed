c770ead12e5e1fab8546970001c291ae503e80ee
Sanpi <sanpi@homecomputing.fr>
2025-12-25 10:19:50 +0100

    Fixes demo sql

--- api/sql/demo.sql
+++ api/sql/demo.sql
@@ -8,9 +8,15 @@ begin
 end
 $$;
 
-insert into "user" (email, password, token)
-    values ('demo', 'demo', uuid_generate_v4())
-    on conflict (email) do nothing;
+drop trigger if exists crypt_user_password on "user";
+
+insert into "user" (user_id, email, password, token)
+    values ('041de296-c518-4005-a068-5989cebfddff', 'demo', crypt(new.password, gen_salt('bf')), uuid_generate_v4())
+    on conflict (user_id) do nothing;
+
+insert into source (user_id, url, title, tags, active) values
+    ('041de296-c518-4005-a068-5989cebfddff', 'https://blog.rust-lang.org/feed.xml', 'Rust blog' '{rust}', true),
+    ('041de296-c518-4005-a068-5989cebfddff', 'https://rustacean-station.org/podcast.rss', 'Rustacean Station' '{rust,podcast}', true);
 
 create or replace function restore_token()
     returns trigger
